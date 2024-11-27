4e16894444afdfd7340189c1866753cb826bc86a
haoyun0 <847360401@qq.com>
2024-11-27 14:22:29 +0800

    重写每周期测G和交易总结

--- plugins/auto_G/stastic.py
+++ plugins/auto_G/stastic.py
@@ -1,22 +1,15 @@
-import asyncio
 import json
-import random
 import re
 from datetime import datetime, timedelta
 
-from nonebot import require, on_command, on_regex
+from nonebot import require, on_regex
 from nonebot.matcher import Matcher
-from nonebot.params import CommandArg, EventPlainText, T_State
+from nonebot.params import EventPlainText
 from nonebot.adapters.onebot.v11 import (
-    MessageEvent,
-    GroupMessageEvent,
-    PrivateMessageEvent,
-    Message,
     Bot
 )
-from ..params.message_api import send_msg, send_msg2
-from ..params.kusa_helper import isSubAccount, isReceiveValid
-from ..params.rule import isInUserList, Message_select_group, isInBotList, PRIVATE
+from ..params.message_api import send_msg
+from ..params.rule import isInUserList, isInBotList, PRIVATE
 from .bank import set_finance
 from nonebot_plugin_apscheduler import scheduler
 
@@ -26,21 +19,26 @@ ceg_group_id = 738721109
 test_group_id = 278660330
 admin_list = [323690346, 847360401, 3584213919, 3345744507]
 GBot = 3345744507
+notice_id = 323690346  # 给谁发通知
+target = ['东', '南', '北', '珠海', '深圳']
+finance = {}
 
 try:
     with open(r'C:/Data/G_data.txt', 'r', encoding='utf-8') as f:
-        data = json.loads(f.read())
+        G_data = json.loads(f.read())
 except:
-    data = {}
+    G_data = {}
 
 
 async def savefile():
     with open(r'C:/Data/G_data.txt', 'w', encoding='utf-8') as s:
-        s.write(json.dumps(data))
+        s.write(json.dumps(G_data))
 
 
 get_G = on_regex(r'^G市有风险，炒G需谨慎！\n.*?\n?当前G值为：\n东校区',
                  rule=PRIVATE() & isInUserList([chu_id]) & isInBotList([GBot]))
+G_conclude = on_regex(r'^您本周期的G市交易总结',
+                      rule=PRIVATE() & isInUserList([chu_id]))
 
 
 @get_G.handle()
@@ -48,83 +46,61 @@ async def handle(matcher: Matcher, bot: Bot, arg: str = EventPlainText()):
     turn_new = 1 if '第一期' in arg else int(re.search(r'当前为本周期第(\d+)期数值。', arg).group(1))
 
     tmp = datetime.now() + timedelta(hours=1) - timedelta(minutes=turn_new * 30)
-    date = tmp.strftime('')
-
-    str1 = f'周期: {turn_new}'
-    str2 = ""
-    for i in range(5):
-        t = target[i]
-        t2 = t + '校区：'
-        tmp = arg.index(t2) + 3 + len(t)
-        if turn_new == 1:
-            value_new = float(arg[tmp: arg.index('\n', tmp)])
-        else:
-            value_new = float(arg[tmp: arg.index('(', tmp)])
-        G_data[i] = value_new
-        if data['turn'] == turn_new:
-            continue
-        str1 += f', {t[0]}: {value_new}'
-        if value_new > data[t]['value_max']:
-            data[t]['value_max'] = value_new
-
-        # if not init_flag and data[t]['own'] > 0:
-        #     if (data[t]['value_max'] - data[t]['value_in']) / data[t]['value_in'] > target_change[i] * 2:
-        #         invest = int(data['invest'] / divide / value_new)
-        #         if data[t]['own'] > invest:
-        #             # x = 1
-        #             # while data[t]['own'] / 2 > invest * (x + 1):
-        #             #     x += 1
-        #             # data[t]['invest_times'] -= x
-        #             # data['invest_total'] -= x
-        #             # str2 += f'\n决策减仓卖出{invest * x}{t[0]}G, 买入价{data[t]["value_in"]}'
-        #             # await G_send_msg(i, False, invest * x, x)
-        #             # data[t]['own'] -= invest * x
-        #             # data[t]['value_max'] = value_new
-        #             # data[t]['value_in'] = value_new
-        #             pass
-        #     elif (data[t]['value_max'] - value_new) / data[t]['value_max'] > target_change[i] * 1:
-        #         if value_new < data[t]['value_last']:
-        #             if data[t]['invest_times'] < divide * 2 and data['invest_total'] < divide * 10:
-        #                 # data[t]['invest_times'] += 1
-        #                 # data['invest_total'] += 1
-        #                 # invest = int(data['invest'] / divide / value_new)
-        #                 # str2 += f'\n决策套牢买入{invest}{t[0]}G, 高峰{data[t]["value_max"]}'
-        #                 # await G_send_msg(i, True, invest)
-        #                 # data[t]['own'] += invest
-        #                 # data[t]['value_max'] = value_new
-        #                 # data[t]['value_in'] = value_new
-        #                 pass
-        # elif init_flag:
-        if init_flag:
-            invest = int(data[t]['coin'] * 2 / value_new)
-            str2 += f'\n决策初始买入{invest}{t[0]}G'
-            await G_send_msg(i, True, invest, divide * 2)
-            data[t]['own'] = invest
-            data[t]['value_max'] = value_new
-            data[t]['value_in'] = value_new
-        data[t]['value_last'] = value_new
-
-    data['update_minute'] = datetime.now().minute
-    data['update_hour'] = datetime.now().hour
-    if data['turn'] == turn_new:
+    date = tmp.strftime("%Y-%m-%d")
+    if date not in G_data:
+        G_data[date] = {}
+    if str(turn_new) in G_data[date]:
         await matcher.finish()
-    data['turn'] = turn_new
 
-    if not init_flag:
-        await bot.send_private_msg(
-            user_id=323690346,
-            message=str1 + str2 + f'\n盈亏估值: '
-                                  f'东南: {round(num_conclude[GBot_list[0]] / 1000000)}m, '
-                                  f'北珠: {round(num_conclude[GBot_list[1]] / 1000000)}m, '
-                                  f'深: {round(num_conclude[GBot_list[2]] / 1000000)}m')
-    else:
-        await bot.send_private_msg(user_id=323690346, message=str1 + f'\n初始化完成，每支G价{data["invest"] * 2}')
-    if init_flag:
-        data['invest_total'] = divide * 10
-        init_flag = False
+    value_all = re.search(r'^G市有风险，炒G需谨慎！\n.*?\n?当前G值为：\n'
+                          r'东校区：(\d+.\d+).*?\n南校区：(\d+.\d+).*?\n北校区：(\d+.\d+).*?\n'
+                          r'珠海校区：(\d+.\d+).*?\n深圳校区：(\d+.\d+).*?\n', arg)
+
+    outputStr = f'周期: {turn_new}'
+    new_data = []
+
+    for i in range(5):
+        value_new = float(value_all.group(i + 1))
+        new_data.append(value_new)
+        outputStr += f', {target[i][0]}: {value_new}'
+
+    G_data[date][str(turn_new)] = new_data
     await savefile()
+
+    try:
+        m = [finance[admin_list[0]], finance[admin_list[1]], finance[admin_list[2]], finance[admin_list[3]]]
+        await set_finance(m)
+        for i in range(4):
+            m[i] = round(m[i] / 1000000)
+        outputStr += f"\n无形: {m[0]}, 有形: {m[1]}, 跟G: {m[2]}, 抄底: {m[3]}"
+    except:
+        pass
+    await send_msg(bot, user_id=notice_id, message=outputStr)
+
     await matcher.finish()
 
 
 async def get_G_data():
-    pass
+    tmp = datetime.now()
+    date = tmp.strftime("%Y-%m-%d")
+    while date not in G_data:
+        tmp -= timedelta(days=1)
+        date = tmp.strftime("%Y-%m-%d")
+    i = 145
+    while str(i) not in G_data[date]:
+        i -= 1
+    return G_data[date][str(i)]
+
+
+@scheduler.scheduled_job('cron', minute='0,30', second=3)
+async def handle():
+    for bot_id in admin_list:
+        await send_msg(bot_id, user_id=chu_id, message='!交易总结')
+
+
+@G_conclude.handle()
+async def handle(matcher: Matcher, bot: Bot, arg: str = EventPlainText()):
+    finance[int(bot.self_id)] = int(re.search(r"本周期盈亏估值：(\d+)草。", arg).group(1))
+    if bot.self_id == str(GBot):
+        await send_msg(bot, user_id=chu_id, message='!测G')
+    await matcher.finish()
